6935bf67d0b8efcf82603770597c203cffdc9210
Alec <starcev.sash@gmail.com>
2024-07-19 15:33:06 +0300

    chore: Update dependencies and import paths

--- packages/eslint-config/src/index.ts
+++ packages/eslint-config/src/index.ts
@@ -1,62 +1,76 @@
-import { presetAll, sxzz } from '@sxzz/eslint-config'
-import pluginSecurity from 'eslint-plugin-security'
-import sonarjs from 'eslint-plugin-sonarjs'
+import antfu from '@antfu/eslint-config'
 
 import type { Linter } from 'eslint'
-const config = sxzz(
-  [
-    pluginSecurity.configs.recommended,
-    ...presetAll,
-    sonarjs.configs.recommended as Linter.FlatConfig,
-    {
-      rules: {
-        'eslint-comments/no-unlimited-disable': 'off',
-        'no-duplicate-imports': 'off',
-        'import/order': [
-          'error',
-          {
-            'newlines-between': 'always',
-            distinctGroup: true,
 
-            groups: [
-              'builtin',
-              'external',
-              'object',
-              'parent',
-              'internal',
-              'sibling',
-              'index',
-              'type',
-            ],
+async function config(): Promise<Linter.FlatConfig[]> {
+  return await antfu({
+    type: 'lib',
+    stylistic: true,
+    rules: {
+      'antfu/if-newline': ['error'],
+      'style/max-statements-per-line': ['off'],
+      'style/array-bracket-newline': ['error', { multiline: true }],
+      'eol-last': ['error', 'always'],
 
-            pathGroups: [
-              {
-                pattern: '@/**',
-                group: 'internal',
-                position: 'after',
-              },
-              {
-                pattern: '~/**',
-                group: 'internal',
-                position: 'after',
-              },
-            ],
+      'import/order': [
+        'error',
+        {
+          'newlines-between': 'always',
+          'distinctGroup': true,
 
-            alphabetize: {
-              order: 'asc',
-              orderImportKind: 'asc',
-              caseInsensitive: false,
+          'groups': [
+            'builtin',
+            'external',
+            'object',
+            'parent',
+            'internal',
+            'sibling',
+            'index',
+            'type',
+          ],
+
+          'pathGroups': [
+            {
+              pattern: '@/**',
+              group: 'internal',
+              position: 'after',
             },
+            {
+              pattern: '~/**',
+              group: 'internal',
+              position: 'after',
+            },
+          ],
+
+          'alphabetize': {
+            order: 'asc',
+            orderImportKind: 'asc',
+            caseInsensitive: false,
           },
-        ],
-      },
+        },
+      ],
+
+      // General
+      'no-console': ['warn', { allow: ['warn', 'error'] }],
+      'no-unused-vars': ['warn', { args: 'none' }],
+      'no-use-before-define': ['error', { functions: false }],
+      'no-param-reassign': ['error', { props: false }],
+      'no-underscore-dangle': ['error', { allow: ['_id', '_count'] }],
+      'no-shadow': ['error', { allow: ['_id', '_count'] }],
+      'no-unused-expressions': ['error', { allowShortCircuit: true }],
+      'no-shadow-restricted-names': ['error'],
+
+      // Stylistic
+      'curly': ['error', 'multi-line', 'consistent'],
+      'newline-before-return': ['error'],
+      'newline-per-chained-call': ['error', { ignoreChainWithDepth: 1 }],
+      'multiline-ternary': ['error', 'always-multiline'],
+      'brace-style': ['error', 'stroustrup'],
+      'arrow-body-style': ['error', 'always'],
+      'eqeqeq': ['error', 'smart'],
+
     },
-  ],
-  {
-    prettier: true,
-    sortKeys: true,
-    command: true,
-  },
-)
+  })
+}
 
 export default config
